3c763f6a298521815f124deeda80448a76170b50
Marc Hanheide <marc@hanheide.net>
2018-05-14 21:46:31 +0000

    fa from cdn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://unpkg.com/vue-multiselect@2.0.6/dist/vue-multiselect.min.css">
         <link rel="stylesheet" href="{{ url(config('app.url_prefix') . mix('css/app.css')) }}">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="tne_base_path" content="{{ route('home') }}">
         <title>Transnational Programmes</title>
